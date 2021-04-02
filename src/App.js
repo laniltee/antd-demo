e9bb43d47ae3263e00bf28e8020656da654af32c
Lanil Marasinghe <lanil@bitbucket.org>
2021-04-02 10:51:23 +0530

    basic color change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Button } from 'antd';
-import './App.css';
+import { Button, Space, Upload, Popconfirm } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import './App.scss';
+
+const onButtonClick = () => {
+  alert("Hello There !!!");
+}
 
 const App = () => (
     <div className="App">
-      <Button type="primary">Button</Button>
+      <Button type="primary" onClick={onButtonClick}>Button</Button>
+      <Space>
+        Space
+        <Button type="primary">Button</Button>
+        <Upload>
+          <Button>
+            <UploadOutlined /> Click to Upload
+          </Button>
+        </Upload>
+        <Popconfirm title="Are you sure delete this task?" okText="Yes" cancelText="No">
+          <Button>Confirm</Button>
+        </Popconfirm>
+      </Space>
     </div>
 );
 
