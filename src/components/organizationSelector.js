a4aa69dc52e8e265ad1a0f9510e739617b53cc8e
Lanil Marasinghe <lanil@bitbucket.org>
2021-09-18 21:05:49 +0530

    added drawyer

--- src/components/organizationSelector.js
+++ src/components/organizationSelector.js
@@ -1,17 +1,45 @@
-import React from 'react'
-import { Select } from 'antd'
-import { Option } from 'antd/es/mentions'
+import React, { useState } from 'react'
+import { Button, Drawer, Select, Tooltip } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
 
 const OrganizationSelector = () => {
+  const [visible, setVisible] = useState(false)
+  const showDrawer = () => {
+    setVisible(true)
+  }
+  const onClose = () => {
+    setVisible(false)
+  }
+
   return (
-    <Select defaultValue="lucy" style={{ width: 120 }}>
-      <Option value="jack">Jack</Option>
-      <Option value="lucy">Lucy</Option>
-      <Option value="disabled" disabled>
-        Disabled
-      </Option>
-      <Option value="Yiminghe">yiminghe</Option>
-    </Select>
+    <>
+      <Select defaultValue="lucy" style={{ width: 120 }}>
+        <Select.Option value="jack">Jack</Select.Option>
+        <Select.Option value="lucy">Lucy</Select.Option>
+        <Select.Option value="disabled" disabled>
+          Disabled
+        </Select.Option>
+        <Select.Option value="Yiminghe">yiminghe</Select.Option>
+      </Select>
+      <Tooltip title="search">
+        <Button
+          onClick={showDrawer}
+          type="primary"
+          shape="circle"
+          icon={<SearchOutlined />}
+        />
+      </Tooltip>
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        onClose={onClose}
+        visible={visible}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
   )
 }
 
